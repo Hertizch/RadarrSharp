21f0c9523fac8252c1cf1f3a392cb9985724944b
Hertizch <ihrstad@gmail.com>
2018-04-17 23:50:59 +0200

    Add release endpoint

---
+++ RadarrSharp/Endpoints/Release/IRelease.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RadarrSharp.Endpoints.Release
+{
+    /// <summary>
+    /// Release endpoint client
+    /// </summary>
+    public interface IRelease
+    {
+        /// <summary>
+        /// Gets releases from Radarr's search cache (30 minute cache)
+        /// </summary>
+        /// <returns></returns>
+        Task<IList<Models.Release>> GetReleases();
+
+        /// <summary>
+        /// Adds a previously searched release to the download client, if the release is still in Radarr's search cache (30 minute cache)
+        /// </summary>
+        /// <param name="guid">Release GUID</param>
+        /// <returns></returns>
+        Task<IList<Models.Release>> AddRelease(string guid);
+    }
+}
---
+++ RadarrSharp/Endpoints/Release/Release.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using RadarrSharp.Helpers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RadarrSharp.Endpoints.Release
+{
+    /// <summary>
+    /// Release endpoint client
+    /// </summary>
+    public class Release : IRelease
+    {
+        private RadarrClient _radarrClient;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Release"/> class.
+        /// </summary>
+        /// <param name="radarrClient">The radarr client.</param>
+        public Release(RadarrClient radarrClient)
+        {
+            _radarrClient = radarrClient;
+        }
+
+        /// <summary>
+        /// Gets releases from Radarr's search cache (30 minute cache)
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IList<Models.Release>> GetReleases()
+        {
+            var json = await _radarrClient.GetJson($"/release");
+
+            if (!string.IsNullOrEmpty(json))
+                return JsonConvert.DeserializeObject<IList<Models.Release>>(json, Converter.Settings);
+
+            return null;
+        }
+
+        /// <summary>
+        /// Adds a previously searched release to the download client, if the release is still in Radarr's search cache (30 minute cache)
+        /// </summary>
+        /// <param name="guid">Release GUID</param>
+        /// <returns></returns>
+        public async Task<IList<Models.Release>> AddRelease(string guid)
+        {
+            string parameter = JsonConvert.SerializeObject(new Dictionary<string, object>
+            {
+                ["guid"] = guid
+            });
+
+            var json = await _radarrClient.PostJson($"/release", parameter, "POST");
+
+            if (!string.IsNullOrEmpty(json))
+                return JsonConvert.DeserializeObject<IList<Models.Release>>(json, Converter.Settings);
+
+            return null;
+        }
+    }
+}
